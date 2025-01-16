051a349f17db2b80c42b006991bd35141f7e8410
Peter Toth <tothpeter08@gmail.com>
2025-01-17 01:00:03 +0700

    Refactor: Encapsulate functionality

--- search_page.js
+++ search_page.js
@@ -1,43 +1,51 @@
-const observer = new MutationObserver((mutations) => {
-  for (const mutation of mutations) {
-    mutation.addedNodes.forEach((element) => {
-      if (
-        element.classList &&
-        element.classList.contains(
-          'property-card-container',
-          'compact',
-          'horizontal'
-        )
-      ) {
-        hijackClick(element);
+class SearchPage {
+  static up() {
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        mutation.addedNodes.forEach((element) => {
+          if (
+            element.classList &&
+            element.classList.contains(
+              'property-card-container',
+              'compact',
+              'horizontal'
+            )
+          ) {
+            SearchPage.hijackPropertyClickOnMap(element);
+          }
+        });
       }
     });
+
+    observer.observe(document, {
+      childList: true,
+      subtree: true,
+    });
   }
-});
 
-observer.observe(document, {
-  childList: true,
-  subtree: true,
-});
+  // Open properties in new tab when they are clicked on the map while holding
+  // the cmd key
+  static hijackPropertyClickOnMap(propertyCardElement) {
+    propertyCardElement
+      .querySelector('.property-photos')
+      .addEventListener('click', SearchPage.clickHandler);
+    propertyCardElement
+      .querySelector('.property-info-container')
+      .addEventListener('click', SearchPage.clickHandler);
+  }
 
-function hijackClick(element) {
-  element
-    .querySelector('.property-photos')
-    .addEventListener('click', clickHandler);
-  element
-    .querySelector('.property-info-container')
-    .addEventListener('click', clickHandler);
+  static clickHandler(e) {
+    // Open the property in the current tab if the user is not holding the meta key
+    if (!e.metaKey) return;
+
+    e.stopPropagation();
+    e.preventDefault();
+
+    const url = document.querySelector(
+      '.property-card-container.horizontal.selected'
+    ).href;
+    window.open(url, '_blank');
+  }
 }
 
-function clickHandler(e) {
-  // Open the property in the current tab if the user is not holding the meta key
-  if (!e.metaKey) return;
-
-  e.stopPropagation();
-  e.preventDefault();
-
-  const url = document.querySelector(
-    '.property-card-container.horizontal.selected'
-  ).href;
-  window.open(url, '_blank');
-}
+SearchPage.up();
