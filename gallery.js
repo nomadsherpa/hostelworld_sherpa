58b8c8a92d4e4c470a216f26d02967a93a25aeb8
Peter Toth <tothpeter08@gmail.com>
2025-01-16 22:14:50 +0700

    Simplify the arrow keys navigation in the gallery logic

--- gallery.js
+++ gallery.js
@@ -19,40 +19,33 @@ class Gallery {
     // Skip if the gallery is not open
     if (!this.galleryContainer.querySelector('.image-lightbox')) return;
 
-    // Hostelworld is using the same aria-label for both next and previous buttons
-    // and it hides the previous button when we are on the first image
-    // and the next button when we are on the one.
-    const buttons = document.querySelectorAll('[aria-label="Next"]');
-    let prevButton, nextButton;
-
-    if (buttons.length === 1) {
-      // The previous sibling of the next button is the image itself
-      // when we are on the first image
-      if (buttons[0].previousElementSibling) {
-        prevButton = null;
-        nextButton = buttons[0];
-      } else {
-        prevButton = buttons[0];
-        nextButton = null;
-      }
-    } else {
-      prevButton = buttons[0];
-      nextButton = buttons[1];
-    }
-
     switch (event.key) {
-      case 'ArrowRight':
-        if (nextButton) {
-          nextButton.click();
-          event.preventDefault();
-        }
-        break;
       case 'ArrowLeft':
-        if (prevButton) {
-          prevButton.click();
-          event.preventDefault();
-        }
+        this.showPreviousImage();
         break;
+      case 'ArrowRight':
+        this.showNextImage();
+        break;
+    }
+  }
+
+  showPreviousImage() {
+    const previousImageButton = this.galleryContainer.querySelector(
+      '.image-lightbox-container > button:first-child'
+    );
+
+    if (previousImageButton) {
+      previousImageButton.click();
+    }
+  }
+
+  showNextImage() {
+    const nextImageButton = this.galleryContainer.querySelector(
+      '.image-lightbox-container > button:last-child'
+    );
+
+    if (nextImageButton) {
+      nextImageButton.click();
     }
   }
 }
