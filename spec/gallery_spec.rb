15f6d2128e02bd02287bdd2d0fab24480baf6e70
Peter Toth <tothpeter08@gmail.com>
2025-01-18 00:43:08 +0700

    Cover the gallery navigation with a test

--- spec/gallery_spec.rb
+++ spec/gallery_spec.rb
@@ -1,8 +1,21 @@
-require "spec_helper"
+describe "Gallery - Arrow navigation", type: :feature do
+  scenario do
+    visit "https://www.hostelworld.com/pwa/wds/hosteldetails.php/Rom-Casa-Hostel-Da-Nang/Da-Nang/278390"
 
-RSpec.describe "Capybara Example", type: :feature do
-  it "visits a page" do
-    visit "https://example.com"
-    expect(page).to have_content("Example Domain")
+    # When the user opens an image in the gallery
+    first(".gallery-slider-item").click
+    first(".gallery-mosaic img").click
+    # Then the first image in the gallery is shown
+    expect(page).to have_css(".image-lightbox-container button", count: 1)
+
+    # When the user presses the right arrow key
+    page.driver.browser.action.send_keys(:arrow_right).perform
+    # Then we show the next image
+    expect(page).to have_css(".image-lightbox-container button", count: 2)
+
+    # When the user presses the left arrow key
+    page.driver.browser.action.send_keys(:arrow_left).perform
+    # Then we show the previous image
+    expect(page).to have_css(".image-lightbox-container button", count: 1)
   end
 end
