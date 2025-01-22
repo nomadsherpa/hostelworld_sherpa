4c634ef33769b087f12ffef59334f82d105c5fd7
Peter Toth <tothpeter08@gmail.com>
2025-01-23 01:25:03 +0700

    Add RSpec debug mode
    If enabled it starts a debug session after the first failure.
    Useful for debugging flaky tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -77,6 +77,15 @@ RSpec.configure do |config|
     MitmdumpProxy.eject_cassette
   end
 
+  if ENV["RSPEC_DEBUG_MODE"] == "true"
+    config.fail_fast = true
+    require "debug"
+
+    config.after do |example|
+      debugger if example.exception
+    end
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
