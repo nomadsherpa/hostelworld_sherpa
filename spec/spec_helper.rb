6e9428e9503b258f2f51ec1853d6ce33068f8564
Peter Toth <tothpeter08@gmail.com>
2025-01-24 11:17:32 +0700

    Restart the Selenium browser between tests
    This is important when two tests use a different Capybara driver.
    Also this stops one test effecting another.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -87,6 +87,10 @@ RSpec.configure do |config|
     end
   end
 
+  config.after do
+    Capybara.current_session.driver.quit
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
